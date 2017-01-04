1f4c91fa1efdd0d2362a026941d99fac11a66ea2
Wang <842876647@qq.com>
2017-01-04 11:14:50 +0800

    添加随机键功能

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,19 +1,51 @@
 /*
- * 密码输入结束重写callFunc
+ *
  */
 var disappearTime = 100; //键盘消失动画时间
 var appearTime = 100; //键盘出现动画时间
 var pwd_Array = []; //密码
-var if_release = true; //清空
 var key_container_h = window.screen.height * 0.55; //键盘&密码框高度
 var keyRect_h = window.screen.height * 0.3; //键盘高度
+var if_release = true; //清空
+var if_random = true; //随机键
+
 (function() {
 	$('.keyCell').each(function(index, obj) {
 		obj.addEventListener('tap', function() {
 			keyEvent(index);
 		})
 	}, null);
-})();
+}());
+
+function getRandomKeyvalue() {
+	if(if_random) {
+		var number_arr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+		var func_arr = ['c', 'del'];
+		var new_arr = [];
+		var circleTimes = number_arr.length;
+		var keycell = $('.keyCell');
+
+		for(var i = 0; i < circleTimes; i++) {
+			var index = Math.floor(Math.random() * number_arr.length);
+			var elem = number_arr[index];
+			new_arr.push(elem);
+			number_arr.splice(index, 1);
+		}
+
+		new_arr.splice(9, 0, func_arr[0]);
+		new_arr.splice(11, 9, func_arr[1]);
+		for(var i = 0; i < keycell.length; i++) {
+			keycell[i].innerText = new_arr[i];
+		}
+
+	} else {
+		var number_arr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'c', '0', 'del'];
+		var keycell = $('.keyCell');
+		for(var i = 0; i < keycell.length; i++) {
+			keycell[i].innerText = number_arr[i];
+		}
+	}
+}
 
 function closeKeyboard() {
 	$('#keyboard_main').animate({
@@ -28,6 +60,7 @@ function closeKeyboard() {
 }
 
 function callKeyboard() {
+	getRandomKeyvalue();
 	$('#keyboard_container').show();
 	$('#keyboard_main').animate({
 		height: key_container_h
@@ -68,7 +101,7 @@ function addNumber(i) {
 		pwd_Array.push(keyValue);
 		drawDot();
 	}
-	pwd_Array.length >= 6 ? callFunc() : '';
+	pwd_Array.length >= 6 ? prePay() : '';
 }
 
 function drawDot() {
@@ -82,9 +115,9 @@ function release() {
 }
 
 function prePay() {
-	console.log('密码输入结束');
+	console.log(pwd_Array);
 }
 
-function forgetPwd(){
+function forgetPwd() {
 	console.log('忘记密码');
 }
